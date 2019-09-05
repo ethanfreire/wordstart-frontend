0368d1e0ed2f36cd7ba321830c89d51d172b87bf
Your Name <you@example.com>
2019-09-05 09:16:02 -0400

    fixing code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import GameContainer from "/Users/ethanfreire/Desktop/wordstart-frontend/src/GameContainer.js"
+import GameContainer from "src/GameContainer.js"
 
 
 function App() {
